41e5cf703798e9f2febeaa4ea57f876bae9a9504
Daniel Kelly <me@danielkelly.io>
2025-04-24 16:38:34 -0500

    2-getting-started-with-nuxt-auth-utils-and-user-sessions

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession();
+</script>
+<template>
+  <div>
+    {{ user }}
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-auth-utils"],
 });
