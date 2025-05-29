ff9a8bad3630cbb6e8a890dc75b84035850e8e1f
Daniel Kelly <me@danielkelly.io>
2025-05-29 11:21:31 -0500

    4-setup-page-with-nuxt-ui-to-login-with-github

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,15 @@
 const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession();
 </script>
 <template>
-  <div>
+  <div v-if="loggedIn">
+    <UButton @click="clear()">Logout</UButton>
     {{ user }}
   </div>
+  <div v-else>
+    <UButton @click="openInPopup('/auth/github')">Login</UButton>
+  </div>
 </template>
+<style>
+@import "tailwindcss";
+@import "@nuxt/ui";
+</style>
